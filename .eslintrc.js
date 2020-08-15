5a9aae4776f2601ec2a544d4972c3ac7d63b6e22
Mujahid Anuar <mujahidfanuar@gmail.com>
2020-08-15 11:12:25 +0800

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    "universe",
+    "universe/native",
+    "universe/shared/typescript-analysis",
+  ],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx", "*.d.ts"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+  ],
+};
